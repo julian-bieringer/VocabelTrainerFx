31698c6a018b073a74a0cf2c7dc8c3c4b6e58a66
Jbiering <Jbiering@Jbiering-PC>
2016-06-08 19:33:18 +0200

    Added method to read all units stored on the local machine

--- src/file/ReadFile.java
+++ src/file/ReadFile.java
@@ -1,6 +1,7 @@
 package file;
 
-import controller.FilePathHelper;
+import helper.FilePathHelper;
+import controller.LessonsWindowController;
 import java.io.IOException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.JsonParseException;
@@ -9,6 +10,10 @@ import java.nio.file.Files;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
 import model.Lesson;
 
@@ -28,5 +33,31 @@ public class ReadFile {
             System.out.println("File does not exist at " + f.toString());
         }
         return lesson;
+    }
+    
+    public static Lesson[] readAllJsonFiles(){
+        List<Lesson> lessons = new ArrayList<>();
+        final File folder = new File(FilePathHelper.getUserDirectory());
+        final File[] listOfFiles = folder.listFiles();
+        File[] array;
+        Lesson first = null;
+        
+        if(listOfFiles != null){
+            for (int length = (array = listOfFiles).length, i = 0; i < length; ++i) {
+                final File file = array[i];
+                if (file.isFile() && !file.getAbsolutePath().contains("VocabelFile")) {
+                    try {
+                        Lesson lesson = ReadFile.readJsonFile(file.getName());
+                        lessons.add(lesson);
+                    } catch (JsonMappingException ex) {
+                        Logger.getLogger(LessonsWindowController.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(LessonsWindowController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }
+        Lesson[] lessonArray = new Lesson[lessons.size()];
+        return lessons.toArray(lessonArray);
     }
 }
